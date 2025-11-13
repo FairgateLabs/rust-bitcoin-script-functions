3e286270571e62f267e97b9edc15ace3b439a8b4
Ignacio Pulice Donatto <ignacio.pulice@fairgate.io>
2025-11-13 16:30:40 -0300

    Change cargo path and remove method

--- src/signatures/winternitz.rs
+++ src/signatures/winternitz.rs
@@ -1,4 +1,3 @@
-use bitcoin::secp256k1::SecretKey;
 use bitcoin_script_stack::stack::{StackTracker, StackVariable};
 pub const DIGITS_PER_BIT: u8 = 4;
 pub const BASE: u8 = 1 << DIGITS_PER_BIT;
@@ -98,34 +97,6 @@ pub fn winternitz_checksig(
             stack.to_altstack();
         }
     }
-}
-
-pub fn get_winternitz_checksig_script(
-    public_keys: &Vec<String>,
-    message_size: u32,
-    max: u8,
-    bits_per_digit: u8,
-    keep_message: bool,
-) -> bitcoin::ScriptBuf {
-    let mut stack = StackTracker::new();
-
-    // Define the public keys and hints in the stack (needed for the stack tracker to work)
-    for i in 0..public_keys.len() {
-        stack.define(1, format!("public_key_{}", i).as_str());
-        stack.define(1, format!("hint_{}", i).as_str());
-    }
-
-    winternitz_checksig(
-        &mut stack,
-        public_keys,
-        message_size,
-        max,
-        bits_per_digit,
-        keep_message,
-    );
-
-    // Return the script from the stack tracker, defined variables are not included in the script.
-    stack.get_script()
 }
 
 #[cfg(test)]
